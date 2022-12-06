c0b21d2bddb37b36e53b2fd6416f7dc4f62b96b2
yiero <goriteo1@hotmail.fr>
2022-12-06 02:15:50 +0100

    animation affinée

--- front/src/components/Test.jsx
+++ front/src/components/Test.jsx
@@ -3,9 +3,14 @@ import { Link } from "react-router-dom"
 import '../styles/Ferrari.css'
 
 function Test () {
+
+    function reload() {
+        window.location.reload();
+    }
     return (
         <main id="main_ferrari">
             <Link to={"/"} id="Test">hello c'est moi</Link>
+            <button onClick={reload}></button>
             <iconify-icon id="ferrari_logo" icon="simple-icons:ferrari" width="100px" height="100px"></iconify-icon>
         </main>
     )
