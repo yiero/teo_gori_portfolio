774f4007b24d328fd1da340c6d180e3cec52344b
yiero <goriteo1@hotmail.fr>
2022-12-06 02:11:32 +0100

    test logo ferrari loading screen

--- front/src/components/Test.jsx
+++ front/src/components/Test.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom"
+import '../styles/Ferrari.css'
 
 function Test () {
     return (
-        <Link to={"/"}>hello c'est moi</Link>
+        <main id="main_ferrari">
+            <Link to={"/"} id="Test">hello c'est moi</Link>
+            <iconify-icon id="ferrari_logo" icon="simple-icons:ferrari" width="100px" height="100px"></iconify-icon>
+        </main>
     )
 }
 
