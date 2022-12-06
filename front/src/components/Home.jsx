08203afb03cd1517157a9ab0a4823175d82297b6
yiero <goriteo1@hotmail.fr>
2022-12-06 11:24:24 +0100

    création du back

--- front/src/components/Home.jsx
+++ front/src/components/Home.jsx
@@ -1,11 +1,13 @@
 import logo from '../logo.svg';
 import '../styles/Home.css';
 import { Link } from 'react-router-dom';
+import imageStussy from '../images/67-679750_stussy-wallpaper-desktop.jpg'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={imageStussy} id="App-image-background"></img>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
