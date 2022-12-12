67b6a269698f908ccf247c100e5422e96afa2b2f
yiero <goriteo1@hotmail.fr>
2022-12-12 22:17:32 +0100

    trier tableau sur les annee OK

--- front/src/components/ClassicCar.jsx
+++ front/src/components/ClassicCar.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import { useEffect, useState } from 'react';
 import '../styles/ClassicCars.css';
 import Header from './Header';
+import { Link } from 'react-router-dom';
 
 function Classic () {
 
-    const [ cars, setCars ] = useState("");
+    const [ cars, setCars ] = useState([]);
 
     useEffect(() => {
         let token = localStorage.getItem('token');
-        fetch("http://localhost:3000/api/classicCar/" + 2, {
+        fetch("http://localhost:3000/api/classicCar", {
             method: "GET",
             headers: { 
                 'Authorization': "BEARER " + token
@@ -22,15 +23,40 @@ function Classic () {
         })
         .then(function(value) {
             setCars(value);
+            console.log(value)
         })
     }, [])
+
+    cars.sort((a, b) => (a.annee > b.annee ? 1 : -1))
 
     return(
         <React.Fragment>
             <Header/>
-            <h1>{cars.nom}</h1>
-            <img src={cars.imageUrl}></img>
-            <h2>{cars.prix} €</h2>
+            <main id="classicPresentation">
+                {/* <button onClick={resultTest} value="test"></button> */}
+                <div>
+                    {cars.map((value, index) => (
+                        <div>
+                            <div id="img_and_present">
+                                <div id="title_description">
+                                    <h1 key={`${value}-${index}`}>{value.nom}</h1>
+                                    <div>{value.description}</div>
+                                </div>
+                                <div>
+                                    <Link to="/ferrari"><img src={value.imageUrl} id="img_car"></img></Link>
+                                </div>
+                            </div>
+                            <div id="fiche_technique">
+                                <div> Année : {value.annee}</div>
+                                <div> Motorisation : {value.moteur}</div>
+                                <div> Puissance : {value.puissance} Ch</div>
+                                <div> Vitesse : {value.vitesse} km/h</div>
+                                <div> Prix : {value.prix} €</div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </main>
         </React.Fragment>
     )
 }
