48faeaa81ba54e34a69e3968f5a637e26fce8e1e
yiero <goriteo1@hotmail.fr>
2022-12-13 22:11:03 +0100

    footer terminé (fontAwesome OK)

--- front/src/components/Footer.jsx
+++ front/src/components/Footer.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import '../styles/Footer.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faInstagram } from "@fortawesome/free-brands-svg-icons"
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons"
+import { faGithub } from "@fortawesome/free-brands-svg-icons"
+
 
 function Footer () {
     return(
@@ -9,15 +15,27 @@ function Footer () {
                 <div id="flex_contact">
                     <div className="footer_contact">
                         <a href="mailto:goriteo1@hotmail.fr" className="icon_style">
-                            <h1>📨</h1>
+                        <FontAwesomeIcon icon={faEnvelope} size="3x" color="white"/>
                         </a>
-                        <div>Email</div>
+                        <div className="icon_text">Email</div>
+                    </div>
+                    <div className="footer_contact">
+                        <a href="https://www.linkedin.com/in/t%C3%A9o-gori-b34000195/" target="_blank" className="icon_style">
+                            <FontAwesomeIcon icon={faLinkedin} size="3x" color="white"/>
+                        </a>
+                        <div className="icon_text">LinkedIn</div>
+                    </div>
+                    <div className="footer_contact">
+                        <a href="https://github.com/yiero?tab=repositories" target="_blank" className="icon_style">
+                            <FontAwesomeIcon icon={faGithub} size="3x" color="white"/>
+                        </a>
+                        <div className="icon_text">Github</div>
                     </div>
                     <div className="footer_contact">
                         <a href="https://www.instagram.com/teogo._/" target="_blank" className="icon_style">
-                            <h1>📸</h1>
+                            <FontAwesomeIcon icon={faInstagram} size="3x" color="white"/>
                         </a>
-                        <div>Instagram</div>
+                        <div className="icon_text">Instagram</div>
                     </div>
                 </div>
             </div>
